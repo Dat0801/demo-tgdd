c63746d29dcfd6f0fcc86d2ff369a6819d541b12
unknown <nttd0801@gmail.com>
2023-12-05 23:30:16 +0700

    final update

--- MyClass/DAO/ProductDAO.cs
+++ MyClass/DAO/ProductDAO.cs
@@ -140,14 +140,14 @@ namespace MyClass.DAO
         {
             
             string query = "insert into Product(ProductName, ProductDes, CatID, BrandID, Price, PromotionPrice, ProductImage, MetaKeywords, Quantity, Warranty, CreatedBy, Active)" +
-                "values('" + product.ProductName + "', N'" + product.ProductDes + "', '" + product.CatID + "', '" + product.BrandID + "','" + product.Price + "', '" + product.PromotionPrice + "', '" + product.ProductImage + "', '" + product.MetaKeywords + "', '" + product.Quantity + "', '" + product.Warranty + "', '" + product.CreatedBy + "' ,'" + product.Active + "')";
+                "values('" + product.ProductName + "', N'" + product.ProductDes + "', '" + product.CatID + "', '" + product.BrandID + "','" + product.Price + "', '" + product.PromotionPrice + "', '" + product.ProductImage + "', N'" + product.MetaKeywords + "', '" + product.Quantity + "', '" + product.Warranty + "', '" + product.CreatedBy + "' ,'" + product.Active + "')";
             int rs = DataProvider.Instance.ExecuteNonQuery(query);
-            string query1 = "insert into DETAILPRO"+
-                "values('" + product.DetailPro.ProductID + "', N'" + product.DetailPro.IDDetail + "', '" + product.DetailPro.ManHinh + "', '" + product.DetailPro.HeDieuHanh + "','" + product.DetailPro.Chip + "', '" + product.DetailPro.RAM + "', '" + product.DetailPro.DungLuong + "', '" + product.DetailPro.KetNoi + "', '" + product.DetailPro.Sim + "', '" + product.DetailPro.CamSau + "', '" + product.DetailPro.CamTruoc + "' ,'" + product.DetailPro.Pin_Sac + "' ,'" + product.DetailPro.Hang + "' ,'" + product.DetailPro.Loai + "')";
-            int rs1 = DataProvider.Instance.ExecuteNonQuery(query);
-            if(rs1==rs)
-                return rs;
-            return rs1;
+            //string query1 = "insert into DETAILPRO"+
+            //    "values('" + product.DetailPro.ProductID + "', N'" + product.DetailPro.IDDetail + "', '" + product.DetailPro.ManHinh + "', '" + product.DetailPro.HeDieuHanh + "','" + product.DetailPro.Chip + "', '" + product.DetailPro.RAM + "', '" + product.DetailPro.DungLuong + "', '" + product.DetailPro.KetNoi + "', '" + product.DetailPro.Sim + "', '" + product.DetailPro.CamSau + "', '" + product.DetailPro.CamTruoc + "' ,'" + product.DetailPro.Pin_Sac + "' ,'" + product.DetailPro.Hang + "' ,'" + product.DetailPro.Loai + "')";
+            //int rs1 = DataProvider.Instance.ExecuteNonQuery(query);
+            //if(rs1==rs)
+            //    return rs;
+            return rs;
         }
 
         public void DeleteProduct(int ProductID)
@@ -164,13 +164,13 @@ namespace MyClass.DAO
             
             string query = "Update Product Set ProductName = '" + product.ProductName + "', ProductDes = N'" + product.ProductDes + "', CatID = '" + product.CatID + "', BrandID = '" + product.BrandID + "', Price = '" + product.Price + "', PromotionPrice = '" + product.PromotionPrice + "', ProductImage = '" + product.ProductImage + "', MetaKeywords = '" + product.MetaKeywords + "', Quantity = '" + product.Quantity + "', Warranty = '" + product.Warranty + "', CreatedBy = '" + product.CreatedBy + "', Active = '" + product.Active + "' " +
                 "where ProductID = '" + ProductID + "' ";
-            string querydetail = "Update DETAILPRO Set ManHinh = '" + product.DetailPro.ManHinh + "', HeDieuHanh = N'" + product.DetailPro.HeDieuHanh + "', Chip = '" + product.DetailPro.Chip + "', RAM = '" + product.DetailPro.RAM + "', DungLuong = '" + product.DetailPro.DungLuong + "', KetNoi = '" + product.DetailPro.KetNoi + "', Sim = '" + product.DetailPro.Sim + "', CamSau = '" + product.DetailPro.CamSau + "', CamTruoc = '" + product.DetailPro.CamTruoc + "', Pin_Sac = '" + product.DetailPro.Pin_Sac + "', Hang = '" + product.DetailPro.Hang + "', Loai = '" + product.DetailPro.Loai + "' " +
-               "where ProductID = '" + ProductID + "' ";
+            //string querydetail = "Update DETAILPRO Set ManHinh = '" + product.DetailPro.ManHinh + "', HeDieuHanh = N'" + product.DetailPro.HeDieuHanh + "', Chip = '" + product.DetailPro.Chip + "', RAM = '" + product.DetailPro.RAM + "', DungLuong = '" + product.DetailPro.DungLuong + "', KetNoi = '" + product.DetailPro.KetNoi + "', Sim = '" + product.DetailPro.Sim + "', CamSau = '" + product.DetailPro.CamSau + "', CamTruoc = '" + product.DetailPro.CamTruoc + "', Pin_Sac = '" + product.DetailPro.Pin_Sac + "', Hang = '" + product.DetailPro.Hang + "', Loai = '" + product.DetailPro.Loai + "' " +
+            //   "where ProductID = '" + ProductID + "' ";
             int rs = DataProvider.Instance.ExecuteNonQuery(query);
-            int rs1 = DataProvider.Instance.ExecuteNonQuery(querydetail);
-            if (rs1 == rs)
-                return rs;
-            return rs1;
+            //int rs1 = DataProvider.Instance.ExecuteNonQuery(querydetail);
+            //if (rs1 == rs)
+            //    return rs;
+            return rs;
         }
         public List<Product> TimKiem(string searchStr)
         {
